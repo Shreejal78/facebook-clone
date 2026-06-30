3173d67f257933afbf1d5dd02a13e96f9907ff1d
Shreejal78 <shreejalbro@gmail.com>
2026-06-30 19:41:57 +0545

    90% validation done

--- assets/js/LC.js
+++ assets/js/LC.js
@@ -1,73 +1,94 @@
-
 function updateLabelStates() {
-    let inpLogin = document.querySelectorAll(".inp-login");
+	let inpLogin = document.querySelectorAll(".inp-login");
 
-    inpLogin.forEach((inp) => {
-        let lbl = inp.parentElement.querySelector(".inp-lbl");
+	inpLogin.forEach((inp) => {
+		let lbl = inp.parentElement.querySelector(".inp-lbl");
 
-        if (lbl) {
-            if (inp.value != "" && inp.value !== "0") {
-                lbl.classList.add("translate");
-            } else {
-                lbl.classList.remove("translate");
-            }
-        }
-    });
+		if (lbl) {
+			if (inp.value != "" && inp.value !== "0") {
+				lbl.classList.add("translate");
+			} else {
+				lbl.classList.remove("translate");
+			}
+		}
+	});
 }
 
 let inputsAndSelects = document.querySelectorAll(".inp-login");
 if (inputsAndSelects.length > 0) {
-    inputsAndSelects.forEach((element) => {
-        element.addEventListener("input", updateLabelStates);
-        element.addEventListener("change", updateLabelStates);
-        element.addEventListener("blur", updateLabelStates); 
-    });
+	inputsAndSelects.forEach((element) => {
+		element.addEventListener("input", updateLabelStates);
+		element.addEventListener("change", updateLabelStates);
+		element.addEventListener("blur", updateLabelStates);
+	});
 }
 
-let day = document.getElementById('day');
+let day = document.getElementById("day");
 let mon = document.getElementById("month");
 let yrs = document.getElementById("year");
 
 if (day && mon && yrs) {
-    let selDay;
-    let dayCount = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    for (let j = 1930; j <= 2026; j++) {
-        yrs.insertAdjacentHTML("beforeend", `<option value="${j}">${j}</option>`);
-    }
-    for (let i = 1; i <= 31; i++) {
-        day.insertAdjacentHTML("beforeend", `<option value="${i}">${i}</option>`);
-    }
+	let selDay;
+	let dayCount = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+	for (let j = 1930; j <= 2026; j++) {
+		yrs.insertAdjacentHTML("beforeend", `<option value="${j}">${j}</option>`);
+	}
+	for (let i = 1; i <= 31; i++) {
+		day.insertAdjacentHTML("beforeend", `<option value="${i}">${i}</option>`);
+	}
 
-    day.addEventListener("change", () => {
-        selDay = day.value;
-    });
-    mon.addEventListener("change", () => {
-        day.innerHTML = "";
-        let a = mon.value;
+	day.addEventListener("change", () => {
+		selDay = day.value;
+	});
+	mon.addEventListener("change", () => {
+		day.innerHTML = "";
+		let a = mon.value;
 
-        day.insertAdjacentHTML("beforeend", `<option value="" disabled selected></option>`);
-        
-        for (let i = 1; i <= dayCount[a]; i++) {
-            if (selDay == i) {
-                day.insertAdjacentHTML("beforeend", `<option value="${i}" selected>${i}</option>`);
-                continue;
-            }
-            day.insertAdjacentHTML("beforeend", `<option value="${i}">${i}</option>`);
-        }
-        
-        if (selDay > dayCount[a]) {
-            day[dayCount[a]].selected = "true";
-            selDay = dayCount[a];
-        }
-        updateLabelStates();
-    });
+		day.insertAdjacentHTML(
+			"beforeend",
+			`<option value="" disabled selected></option>`,
+		);
+
+		for (let i = 1; i <= dayCount[a]; i++) {
+			if (selDay == i) {
+				day.insertAdjacentHTML(
+					"beforeend",
+					`<option value="${i}" selected>${i}</option>`,
+				);
+				continue;
+			}
+			day.insertAdjacentHTML("beforeend", `<option value="${i}">${i}</option>`);
+		}
+
+		if (selDay > dayCount[a]) {
+			day[dayCount[a]].selected = "true";
+			selDay = dayCount[a];
+		}
+		updateLabelStates();
+	});
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-    updateLabelStates();
+	updateLabelStates();
 });
 
+//Form Validate#
 
-//Form Validate
+let email = document.getElementById("e-num");
+let pass = document.getElementById("pass");
+let form = document.querySelector(".LCform");
+function showError(par, msg) {
+	const oldError = par.querySelector(".errorMsg");
+	if (oldError) {
+		return;
+	} else {
+		par.insertAdjacentHTML("beforeend", `<p class='errorMsg'>${msg}</p>`);
+	}
+}
 
-console.log(document.getElementById('e-num'))
+function removeError(par) {
+	const oldError = par.querySelector(".errorMsg");
+	if (oldError) {
+		oldError.remove();
+	}
+}
